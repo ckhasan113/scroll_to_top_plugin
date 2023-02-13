e3f867185721403332a61ff244a50a9b61fa0af9
Hasan <ck.hasan113@gmail.com>
2023-02-13 14:03:03 +0600

    first

---
+++ simple-scroll-to-top-ck.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Scroll To Top CK
+ * Plugin URI:        https://wordpress.org/plugins/simple-scroll-to-top-ck/
+ * description:       Simple scroll to top plugin will help you to enable back to top button to your wordpress website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires at PHP:   7.2
+ * Author:            Md Al Mehedi Hasan
+ * License:           GPLv2 or later
+ * Text Domain:       sstt 
+ * Update URI:        https://github.com/ckhasan113/scroll_to_top_plugin
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ // sstt - Simple Scroll To Top
+
+?>
